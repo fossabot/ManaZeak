5ddc8bbfa1df4afeed9a7f614034bfaa7608fd88
Squadella <pbouniol@et.esiea.fr>
2018-01-26 16:08:30 +0100

    Updated script.

--- script.sh
+++ script.sh
@@ -1,27 +1,21 @@
 #!/bin/bash
 
-
-
-if [ $1 = "dev" ]
-then
-#command="npm run dev"
-command=`npm run dev`
+if [ $1 = "dev" ]; then
+$command = `docker-compose up -d`
+echo "$command"
+$command = `npm run dev`
 echo "$command"
 
-elif [ $1 = "prod" ]
-then
-command= `npm run prod`
+elif [ $1 = "prod" ]; then
+$command = `docker-compose up -d`
+echo "$command"
+$command = `npm run prod`
 echo "$command"
 
-elif [ $1 = "init" ]
-then
-command= `docker-compose build`
-command= `npm install`
-command= `docker-compose up -d`
+elif [ $1 = "init" ]; then
+$command = `docker-compose build`
+echo "$command"
+$command = `npm install`
 echo "$command"
 
 fi
-
-#npm install
-#docker build
-#docker compose up- d
