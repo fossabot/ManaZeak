48d5a3b0e26e394e2f0c9f3a0860a95ffa87c489
Valentin Peiro <v.peiro@laposte.net>
2018-04-14 19:09:06 +0200

    Add wrappers to add/removeEventListeners for convenience

--- static/js/utils/Overrides.js
+++ static/js/utils/Overrides.js
@@ -22,6 +22,73 @@ Event.prototype.stop = function() {
 };
 
 
+(function track_event_listeners()
+{
+    let original_add = Element.prototype.addEventListener;
+    let original_rem = Element.prototype.removeEventListener;
+
+    let validate_options = function(a, b)
+    {
+        if(a == null || b == null)
+            return true;
+        //This returns false if and only if a and b have a common option which is different
+        for(let i in a)
+            if(b[i] != null && b[i] != a[i])
+                return false;
+        for(let i in b)
+            if(a[i] != null && a[i] != b[i])
+                return false;
+        return true;
+    };
+
+    //This keeps track of the event listeners
+    Element.prototype.addEventListener = function(type, fct, options)
+    {
+        //This makes testing options a lot easier
+        if(typeof options == "boolean")
+            options = {capture: options};
+
+        let signature = {
+            type: type,
+            fct: fct,
+            options: options
+        };
+        if(!this.__listenersList__)
+            this.__listenersList__ = [signature];
+        else
+            this.__listenersList__.push(signature);
+
+        console.log(original_add);
+        original_add.call(this, type, fct, options);
+    };
+
+    //This function can be called with null as arguments to match all listeners
+    Element.prototype.removeEventListener = function(type, fct, options)
+    {
+        if(this.__listenersList__ == null)
+            return;
+
+        if(typeof options == "boolean")
+            options = {capture: options};
+
+        let signature;
+        for(let i = this.__listenersList__.length; i--;)
+        {
+            if(this.__listenersList__[i] == null)
+                continue;
+
+            signature = this.__listenersList__[i];
+            if(type == null || type == signature.type)
+                if(fct == null || fct == signature.fct)
+                    if(validate_options(options, signature.options)) {
+                        original_rem.call(this, signature.type, signature.fct, signature.options);
+                        this.__listenersList__[i] = null;
+                    }
+        }
+    };
+}());
+
+
 (function forceStop() {
     let addEvent = Element.prototype.addEventListener;
     Element.prototype.addEventListener = function(type, handler, options) {
